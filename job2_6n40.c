f53536ebc64f63ea7366cd6c8e49776763977120
Your Name <you@example.com>
2025-06-23 10:50:27 +0700

    First commit

---
+++ job2_6n40.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2,num3,num4,num5,av=0;
+    printf("Enter number : ");
+    scanf("%d",&num1);
+    printf("Enter number : ");
+    scanf("%d",&num2);
+    printf("Enter number : ");
+    scanf("%d",&num3);
+    printf("Enter number : ");
+    scanf("%d",&num4);
+    printf("Enter number : ");
+    scanf("%d",&num5);
+    av=(num1+num2+num3+num4+num5)/5;
+    printf("av : %d" , av);
+}
